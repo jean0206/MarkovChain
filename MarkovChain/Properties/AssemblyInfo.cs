86bf8704ba5fc92f670616910460839d03292f79
jean0206 <jeanortiz00@gmail.com>
2019-09-24 16:45:48 -0500

    project start

---
+++ MarkovChain/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada con un ensamblado.
+[assembly: AssemblyTitle("MarkovChain")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("MarkovChain")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("42e9ee92-489c-43f1-827f-c027a1c7a9af")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Modelo/Matriz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Modelo
+{
+    public class Matriz
+    {
+        public double[][] multiplicarMatriz(double[][] matriz, int exponenete) {
+            double[][] matrizTem = matriz;
+            double n = matriz[0].Length;
+            // filas de la matriz A
+            double m = matriz.Length;
+            // filas de la matriz B
+            double n2 = matriz.Length;
+            // columnas de la matriz B
+            double o = matriz[0].Length;
+            // nueva matriz 
+            double[][] c = new double[m][o];
+            // se comprueba si las matrices se pueden multiplicar
+           
+                for (int i = 0; i < n - 1; i++)
+                {
+                    int a = 0;
+                    for (int j = 0; j < n2 - 1; j++)
+                    {
+                        //aqui se multiplica la matriz
+                        a = a + (matriz[i][j] * B[j][i]);
+                    }
+
+                }
+            
+            /**
+             *  si no se cumple la condición se retorna una matriz vacía
+             */
+            return c;
+            return null;
+        }
+        
+    }
+
+}
